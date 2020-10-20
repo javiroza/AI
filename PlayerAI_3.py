397f05f99f4c3fb31d3e16aa4bc13354daba1791
Javier <javiroza11@gmail.com>
2020-10-21 00:49:32 +0200

    late night

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -1,69 +1,70 @@
 from random import randint
 from BaseAI_3 import BaseAI
-import sys
-
-sys.setrecursionlimit(10**9)
+import time
 
 class PlayerAI(BaseAI): #It takes the class BaseAI as parent
-    def getMove(self, grid):	
-    	""" returns the next move in a number"""
-    	(move,utility) = self.maximize(grid,-1.3)
-    	return move
+	def __init__(self):
+		self.t_0 = time.perf_counter()
 
-    def maximize(self, grid, move):
-    	"""returns tuple of (grid, utility)"""
-    	if self.terminal_test(grid):
-    		return (move, self.utility(grid))
+	def getMove(self, grid):	
+		""" returns the next move in a number"""
+		(move,utility) = self.maximize(grid,1)
+		return move
 
-    	(maxchild,maxutility) = (None,-1.0) 
+	def maximize(self, grid, move):
+		"""returns tuple of (grid, utility)"""
+		if self.terminal_test(grid):
+			return (move, self.utility(grid))
 
-    	grids = self.children(grid)[0::2]
-    	moves = self.children(grid)[1::2]
-    	for child_grid in grids:
-    		child_move = moves[grids.index(child_grid)]
-    		(childprima, utility) = self.minimize(child_grid,child_move)
-    		if utility > maxutility:
-    			(maxchild,maxutility) = (child_move, utility)
+		(maxchild,maxutility) = (None,-1.0) 
 
-    	return (maxchild,maxutility)
+		grids = self.children(grid)[0::2]
+		moves = self.children(grid)[1::2]
+		for child_grid in grids:
+			child_move = moves[grids.index(child_grid)]
+			(childprima, utility) = self.minimize(child_grid,child_move)
+			if utility > maxutility:
+				(maxchild,maxutility) = (child_move, utility)
 
-    def minimize(self, grid, move):
-    	"""returns tuple of (grid, utility)"""
-    	if self.terminal_test(grid):
-    		return (move, self.utility(grid))
+		return (maxchild,maxutility)
 
-    	(minchild,minutility) = (None,1e10) 
+	def minimize(self, grid, move):
+		"""returns tuple of (grid, utility)"""
+		if self.terminal_test(grid):
+			return (move, self.utility(grid))
 
-    	grids = self.children(grid)[0::2]
-    	moves = self.children(grid)[1::2]
-    	for child_grid in grids:
-    		child_move = moves[grids.index(child_grid)]
-    		(childprima, utility) = self.maximize(child_grid,child_move)
-    		if utility < minutilty:
-    			(minchild,minutility) = (child_move, utility)
+		(minchild,minutility) = (None,1e10) 
 
-    	return (minchild,minutility)
+		grids = self.children(grid)[0::2]
+		moves = self.children(grid)[1::2]
+		for child_grid in grids:
+			child_move = moves[grids.index(child_grid)]
+			(childprima, utility) = self.maximize(child_grid,child_move)
+			if utility < minutility:
+				(minchild,minutility) = (child_move, utility)
+
+		return (minchild,minutility)
 
 # the next two methods are defined exclusively for clarity reasons
-    def utility(self, grid):
-    	"""Returns the tile with the maximum value"""
-    	return grid.getMaxTile() 
+	def utility(self, grid):
+		"""Returns the tile with the maximum value"""
+		return grid.getMaxTile() 
 
-    def terminal_test(self, grid):
-    	"""Returns False if the given grid is a loss and True otherwise"""
-    	return not grid.canMove()
+	def terminal_test(self, grid):
+		"""Returns False if the given grid is a loss and True otherwise"""
+		return not grid.canMove()
 
-    def children(self, grid):
-    	"""Returns a list with the children nodes of a given grid"""
-    	children = []
+	def children(self, grid):
+		"""Returns a list with the children nodes of a given grid"""
+		children = []
 
-    	for move in grid.getAvailableMoves():
-    		gridcopy = grid.clone()
-    		gridcopy.move(move)
-    		children.append(gridcopy)
-    		children.append(move) 
+		for move in grid.getAvailableMoves():
+			gridcopy = grid.clone()
+			gridcopy.move(move)
+			children.append(gridcopy)
+			children.append(move) 
 
-    	return children
+		return children
 
 
 
