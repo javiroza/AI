81ab4dcc6d392bfc27ccac9f310658111bcc6a0f
Javier <javiroza11@gmail.com>
2020-10-21 18:01:30 +0200

    before casa laura x si a caso

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -4,16 +4,18 @@ import time
 
 class PlayerAI(BaseAI): #It takes the class BaseAI as parent
 	def __init__(self):
-		self.t_0 = time.perf_counter()
+		self.alpha = -1
+		self.beta = 10000
 
 	def getMove(self, grid):	
 		""" returns the next move in a number"""
-		(move,utility) = self.maximize(grid,1)
+		(move,utility) = self.maximize(grid,None,0,time.perf_counter())
 		return move
 
-	def maximize(self, grid, move):
+	def maximize(self, grid, move, counter, ctime):
 		"""returns tuple of (grid, utility)"""
-		if self.terminal_test(grid):
+		counter += 1
+		if self.terminal_test(grid) or counter > 5:
 			return (move, self.utility(grid))
 
 		(maxchild,maxutility) = (None,-1.0) 
@@ -21,16 +23,24 @@ class PlayerAI(BaseAI): #It takes the class BaseAI as parent
 		grids = self.children(grid)[0::2]
 		moves = self.children(grid)[1::2]
 		for child_grid in grids:
-			child_move = moves[grids.index(child_grid)]
-			(childprima, utility) = self.minimize(child_grid,child_move)
-			if utility > maxutility:
-				(maxchild,maxutility) = (child_move, utility)
+			if (ctime-time.perf_counter() <= 10):
+				child_move = moves[grids.index(child_grid)]
+				(childprima, utility) = self.minimize(child_grid,child_move,counter, time.perf_counter())
+				if utility > maxutility:
+					(maxchild,maxutility) = (child_move, utility)
+			else:
+				return (move, self.utility(grid))
+			if maxutility >= self.beta:
+				break
+			if maxutility > self.alpha:
+				self.alpha = maxutility
 
 		return (maxchild,maxutility)
 
-	def minimize(self, grid, move):
+	def minimize(self, grid, move, counter,ctime):
 		"""returns tuple of (grid, utility)"""
-		if self.terminal_test(grid):
+		counter += 1
+		if self.terminal_test(grid) or counter > 5:
 			return (move, self.utility(grid))
 
 		(minchild,minutility) = (None,1e10) 
@@ -38,10 +48,18 @@ class PlayerAI(BaseAI): #It takes the class BaseAI as parent
 		grids = self.children(grid)[0::2]
 		moves = self.children(grid)[1::2]
 		for child_grid in grids:
-			child_move = moves[grids.index(child_grid)]
-			(childprima, utility) = self.maximize(child_grid,child_move)
-			if utility < minutility:
-				(minchild,minutility) = (child_move, utility)
+			if (ctime-time.perf_counter() <= 10):
+				child_move = moves[grids.index(child_grid)]
+				(childprima, utility) = self.maximize(child_grid,child_move,counter,time.perf_counter())
+				if utility < minutility:
+					(minchild,minutility) = (child_move, utility)
+			else:
+				return(move, self.utility(grid))
+			if minutility <= self.alpha:
+				break
+			if minutility > self.beta:
+				self.beta = minutility
+
 
 		return (minchild,minutility)
 
@@ -66,5 +84,11 @@ class PlayerAI(BaseAI): #It takes the class BaseAI as parent
 
 		return children
 
+############ Notes ###############
+# Without alpha-beta pruning we can see that it hardly ever gets to do
+# a "right" or "left" move, since they are checked last.
+
+# Whith alpha-beta pruning we see many more of the moves mentioned above,
+# which means that it can do so within the time limit.
 
 
